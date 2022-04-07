6e4b8b13e15b8c77b0d0e97fcc09fda34403c969
ambiti0n <fabulousfresh010@gmail.com>
2022-04-07 21:35:05 +0100

    header component / footer component / routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import "./default.scss";
-import Header from "./components/Header";
 import Homepage from "./page/Homepage";
+import { Routes, Route } from "react-router";
+import Registration from "./page/Registration";
+import HomepageLayout from "./layouts/HomepageLayout";
+import MainLayout from "./layouts/MainLayout";
 
+import { Outlet } from "react-router-dom";
+
+const MainLayouts = () => (
+  <>
+    <MainLayout>
+      <Outlet />
+    </MainLayout>
+  </>
+);
+const HomepageLayouts = () => (
+  <>
+    <HomepageLayout>
+      <Outlet />
+    </HomepageLayout>
+  </>
+);
 function App() {
   return (
     <div className="App">
       <div className="main">
-        <Header />
-        <Homepage />
+        <Routes>
+          <Route path="/" element={<HomepageLayouts />}>
+            <Route path="/" element={<Homepage />} />
+          </Route>
+          <Route path="/registeration" element={<MainLayouts />}>
+            <Route path="/registeration" element={<Registration />} />
+          </Route>
+        </Routes>
       </div>
     </div>
   );
---
+++ src/page/Registration/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+// import Signup from "./../../components/Signup";
+import "./styles.scss";
+
+const Registration = (props) => {
+  return <h2> Registration</h2>;
+};
+
+export default Registration;
